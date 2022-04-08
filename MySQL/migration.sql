94f468c5b3dc1d6eb4c45a160004e21d82b9faff
Cody Hodges <hodges.cody00@gmail.com>
2022-04-08 10:28:47 -0500

    Tables

--- MySQL/migration.sql
+++ MySQL/migration.sql
@@ -2,3 +2,53 @@ use ComputerPartsRoom_db;
 
 CREATE DATABASE IF NOT EXISTS ComputerPartsRoom_db;
 
+CREATE TABLE IF NOT EXISTS users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(100),
+  email VARCHAR(100),
+  password varchar(100),
+  PRIMARY KEY (id),
+  UNIQUE (username, email)
+);
+
+
+CREATE TABLE IF NOT EXISTS categories(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50)
+);
+
+
+CREATE TABLE IF NOT EXISTS posts (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id INT UNSIGNED NOT NULL,
+  title VARCHAR(100),
+  content VARCHAR(500),
+  cat_id INT UNSIGNED NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (cat_id) REFERENCES categories (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS profPhoto (
+    id INT UNSIGNED NOT NULL,
+    url VARCHAR(500),
+    user_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS postCategories (
+    post_id INT UNSIGNED NOT NULL,
+    cat_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES posts (id),
+    FOREIGN KEY (cat_id) REFERENCES categories (id)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    sent_id INT UNSIGNED NOT NULL,
+    received_id INT UNSIGNED NOT NULL,
+    content VARCHAR(500),
+    FOREIGN KEY (sent_id) REFERENCES users (id),
+    FOREIGN KEY (received_id) REFERENCES users (id)
+);
