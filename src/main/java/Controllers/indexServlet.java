bbcd6ad94238e51844d7211b912d0b86fd4a0faa
Cody Hodges <hodges.cody00@gmail.com>
2022-04-08 12:03:58 -0500

    starting

--- src/main/java/Controllers/indexServlet.java
+++ src/main/java/Controllers/indexServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads")
+@WebServlet(name = "controllers.indexServlet", urlPatterns = "/")
 public class indexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("posts", DaoFactory.getPostsDao().all());
