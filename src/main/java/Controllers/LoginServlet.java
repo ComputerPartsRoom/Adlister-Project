a2fc528213dd42829c10d391f8d31956b21c54a5
Cody Hodges <hodges.cody00@gmail.com>
2022-04-09 02:03:32 -0500

    Fixed DAO files, Repathed Servlets

--- src/main/java/Controllers/LoginServlet.java
+++ src/main/java/Controllers/LoginServlet.java
@@ -26,16 +26,12 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
         User user = DaoFactory.getUsersDao().findByUsername(username);
 
-        // TODO: find a record in your database that matches the submitted password
-        // TODO: make sure we find a user with that username
-        // TODO: check the submitted password against what you have in your database
         boolean validAttempt = false;
         if (user.getPassword().equals(password)){
             validAttempt = true;
         }
 
         if (validAttempt) {
-            // TODO: store the logged in user object in the session, instead of just the username
             request.getSession().setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
--- src/main/java/Controllers/LogoutServlet.java
+++ src/main/java/Controllers/LogoutServlet.java
@@ -1,4 +1,4 @@
-package com.codeup.adlister.controllers;
+package Controllers;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
