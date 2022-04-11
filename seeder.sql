1bc4b61a6a88f1fee849dbc5dc9c5222ac33ac8a
Cody Hodges <hodges.cody00@gmail.com>
2022-04-10 19:59:56 -0500

    DB Tweaks

--- seeder.sql
+++ seeder.sql
@@ -1,29 +1,45 @@
 use ComputerPartsRoom_db;
 
 INSERT INTO users(username, email, password)
-VALUES ( 'bob', 'bvcyuwd@email.com', 'bcwiuvbc'),
+VALUES ('bob', 'bvcyuwd@email.com', 'bcwiuvbc'),
        ('tom', 'vebrver@email.com', 'cenruic')
 ;
-INSERT INTO categories(name)
- VALUES ( 'books')
-;
+INSERT INTO categories (name)
+VALUES ( 'Memory'),
+    ('Peripherals'),
+    ('Processors'),
+    ('Motherboards'),
+    ('Graphics Cards'),
+    ('Storage') ;
+
+
 INSERT INTO posts(user_id, title, content, cat_id)
-VALUES (1, 'book', 'for sale', 1 )
-;
+VALUES (1, '500GB SSD', 'SunDisk $500', 1 ),
+     (1, '2TB HD', 'SunDisk $300', 1 );
 
 INSERT INTO postCategories(post_id, cat_id)
-VALUES (1, 1 )
-;
-INSERT INTO profPhoto(url, user_id)
-VALUES ('Assets/PlaceHolder.jpeg', 1 )
-;
-INSERT INTO messages(sent_id, received_id, content)
-VALUES ( 1, 2, 'hello')
-;
+VALUES (1, 1 ),
+(2, 1 ),
+(2, 2 ),
+(1, 5 ) ;
 
-SELECT * FROM users;
-SELECT * FROM posts;
-SELECT * FROM profPhoto;
-SELECT * FROM categories;
-SELECT * FROM postCategories;
-SELECT * FROM messages;
+
+INSERT INTO profPhoto(url, user_id)
+VALUES ('Assets/PlaceHolder.jpeg', 1 ) ;
+
+
+INSERT INTO messages(sent_id, received_id, content)
+VALUES ( 1, 2, 'hello') ;
+
+SELECT *
+FROM users;
+SELECT *
+FROM posts;
+SELECT *
+FROM profPhoto;
+SELECT *
+FROM categories;
+SELECT *
+FROM postCategories;
+SELECT *
+FROM messages;
