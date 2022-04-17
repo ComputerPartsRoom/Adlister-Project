9d97e5acb2fe9402228b3e144f7daf07ca733477
Jonathan Clark <jonathan.b.clark1127@gmail.com>
2022-04-17 14:31:39 -0500

    fixed sort and search and logout after edit bug

--- src/main/java/DAO/MySQLPostsDao.java
+++ src/main/java/DAO/MySQLPostsDao.java
@@ -28,7 +28,7 @@ public class MySQLPostsDao implements Posts {
     public List<Post> all() {
         PreparedStatement stmt = null;
         try {
-            stmt = connection.prepareStatement("SELECT posts.id, posts.user_id, posts.title, posts.content, posts.price, posts.img, posts.cat_id, categories.name, posts.username\n" +
+            stmt = connection.prepareStatement("SELECT posts.id, posts.user_id, posts.title, posts.content, posts.price, posts.img, posts.cat_id, categories.name\n" +
                     "FROM posts\n" +
                     "         INNER JOIN categories on posts.cat_id = categories.id;");
             ResultSet rs = stmt.executeQuery();
@@ -83,16 +83,14 @@ public class MySQLPostsDao implements Posts {
     @Override
     public Long insert(Post post) {
         try {
-            String insertQuery = "INSERT INTO posts(user_id, title, content, price, img, cat_id, username) VALUES (?, ?, ?, ?, ?, ?, ?);";
+            String insertQuery = "INSERT INTO posts(user_id, title, content, price, cat_id, img) VALUES (?, ?, ?, ?, ?, ?);";
             PreparedStatement stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
             stmt.setLong(1, post.getUser_id());
             stmt.setString(2, post.getTitle());
             stmt.setString(3, post.getContent());
             stmt.setLong(4, post.getPrice());
-            stmt.setString(5, post.getImg());
-            stmt.setLong(6, post.getCat_id());
-            stmt.setString(7, post.getUsername());
-
+            stmt.setLong(5, post.getCat_id());
+            stmt.setString(6, post.getImg());
             stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
@@ -111,8 +109,7 @@ public class MySQLPostsDao implements Posts {
                 rs.getInt("price"),
                 rs.getLong("cat_id"),
                 rs.getString("img"),
-                rs.getString("name"),
-                rs.getString("username")
+                rs.getString("name")
         );
     }
 
@@ -127,16 +124,17 @@ public class MySQLPostsDao implements Posts {
 
     public void update(Post post) {
         try {
-            String updateQuery = "UPDATE posts SET title=?, content=?, price=?, img=?, cat_id=?, username=? WHERE id=?;";
+            String updateQuery = "UPDATE posts SET title=?, content=?, price=?, img=?, cat_id=? WHERE id=?;";
 
             PreparedStatement stmt = connection.prepareStatement(updateQuery, Statement.RETURN_GENERATED_KEYS);
+            System.out.println("post.getId() = " + post.getId());
             stmt.setString(1, post.getTitle());
             stmt.setString(2, post.getContent());
             stmt.setLong(3, post.getPrice());
             stmt.setString(4, post.getImg());
             stmt.setLong(5, post.getCat_id());
-            stmt.setString(6, post.getUsername());
-            stmt.setLong(7, post.getId());
+            stmt.setLong(6, post.getId());
+
             stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
